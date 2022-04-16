b45209d22a0c406f27247a132a71ced47e642389
ilshatcraft <ilshatcraft@gmail.com>
2022-04-16 21:51:28 +0300

    start
    подготовил проект для написания приложения, установил нужные библиотеки к реакту.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Routes,Route,Link} from "react-router-dom";
+import Home from './pages/Home';
+
+import {React,useState} from 'react'; 
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  return <Router >
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+     
+    </Routes>
+  </Router>;
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+
+const Home = () => {
+
+    return (
+        <div className='homePage'>
+      sd
+        </div>
+    );
+};
+
+export default Home;
